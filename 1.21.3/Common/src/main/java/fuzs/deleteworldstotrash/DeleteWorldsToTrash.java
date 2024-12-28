357dc8089df4093dfb95a274c5a162c6e5627e40
Fuzss <28218241+Fuzss@users.noreply.github.com>
2024-12-28 22:28:09 +0100

    prepare 1.21.3 port

---
+++ 1.21.3/Common/src/main/java/fuzs/deleteworldstotrash/DeleteWorldsToTrash.java
@@ -0,0 +1,15 @@
+package fuzs.deleteworldstotrash;
+
+import net.minecraft.resources.ResourceLocation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DeleteWorldsToTrash {
+    public static final String MOD_ID = "deleteworldstotrash";
+    public static final String MOD_NAME = "Delete Worlds To Trash";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_NAME);
+
+    public static ResourceLocation id(String path) {
+        return ResourceLocation.fromNamespaceAndPath(MOD_ID, path);
+    }
+}
